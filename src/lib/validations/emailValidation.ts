95fb44d7aafd8942fd014ecde42f934748e5a769
Fahim BELLILI <f.bellili.ext@quadient.com>
2021-03-09 20:06:34 +0100

    Added validation

---
+++ src/lib/validations/emailValidation.ts
@@ -0,0 +1,7 @@
+function emailValidation(email: string): boolean {
+  // eslint-disable-next-line no-useless-escape
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+}
+
+export default emailValidation;
